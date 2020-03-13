3f8de4ac646f5babe2a04819ec965f91b5f1cc47
chouyoun <chouyoun@foxmail.com>
2020-03-13 22:24:15 +0800

    fuck

---
+++ MyContent/Finders.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyDm;
+
+namespace MyContent
+{
+    public static class Finders
+    {
+        public static Func<bool> JingjiRedPoint = () =>
+        {
+            return MyDmSoft.CurrentThreadDm.FindPic("竞技红点", new Rectangle(196, 566, 258, 640));
+        };
+        public static Func<bool> YezhanPk = () =>
+        {
+            return MyDmSoft.CurrentThreadDm.FindPic("每日奖励", new Rectangle(53, 230, 150, 269));
+        };
+        public static Func<bool> YezhanPkRePoint = () =>
+        {
+            return MyDmSoft.CurrentThreadDm.FindPic("野战PK红点", new Rectangle(1, 664, 124, 731));
+        };
+        public static Func<bool> MainView = () =>
+        {
+            return MyDmSoft.CurrentThreadDm.FindPic("自动挑战", new Rectangle(399, 557, 496, 605));
+        };
+        public static Func<bool> WangzhezhenbaRedPoint = () =>
+        {
+            return MyDmSoft.CurrentThreadDm.FindPic("王者争霸红点", new Rectangle(113, 668, 238, 724));
+        };
+        public static Func<bool> Pipeiduishou = () =>
+        {
+            return MyDmSoft.CurrentThreadDm.FindPic("匹配对手", new Rectangle(172, 620, 339, 669));
+        };
+        public static Func<bool> Qianwantiaozhan = () =>
+        {
+            return MyDmSoft.CurrentThreadDm.FindPic("前往挑战", new Rectangle(100, 620, 253, 668));
+        };
+        public static Func<bool> Zidonzhandou = () =>
+        {
+            return MyDmSoft.CurrentThreadDm.FindPic("自动战斗", new Rectangle(418, 537, 498, 609));
+        };
+        public static Func<bool> Tingzhizhandou = () =>
+        {
+            return MyDmSoft.CurrentThreadDm.FindPic("停止战斗", new Rectangle(418, 537, 498, 609));
+        };
+        public static Func<bool> Tiaozhan = () =>
+        {
+            return BaiduOcr.OcrByCapture(new Rectangle(398,605,447,630)).RegexMatch("挑|战");
+        };
+        public static Func<bool> Zidonfuhuo = () =>
+        {
+            return BaiduOcr.OcrByCapture(new Rectangle(338, 614, 397, 636)).RegexMatch("自|动|复|活");
+        };
+    }
+}
---
+++ MyContent/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Baymax;
+using MyDm;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace MyContent
+{
+    /*
+     * 点击挑战BOSS以后，有黑屏显示未刷新的情况，要关掉重新打开
+     */
+
+    [Description("王城争霸超V版脚本1.0")]
+    public partial class Program : IContent
+    {
+        public void Main(string[] args)
+        {
+            this.init();
+
+            Finders.Tiaozhan();
+
+            //mojie();
+            return;
+            do
+            {
+                jingji();
+
+
+                Thread.Sleep(500);
+            } while (true);
+            
+            Controller.ViewPrintLine("ok!");
+        }
+
+        void mojie()
+        {
+            Waiting waiting = new Waiting();
+
+            //点击BOOS
+            waiting.BackClick(454, 406);
+            Thread.Sleep(500);
+
+            //等待前往挑战
+            waiting.SetFinder(Finders.Qianwantiaozhan).WaitShow(null,waiting.NewBackChecker(Finders.MainView));
+            Thread.Sleep(500);
+
+            //点击十倍爆率
+            this.dm.Click(433, 188);
+            Thread.Sleep(500);
+
+            //点击前往挑战
+            waiting.BackClickFoundPoint();
+            Thread.Sleep(500);
+
+            //等待进入
+            waiting.SetFinder(Finders.Zidonzhandou).WaitShow(null, waiting.BackChecker);
+            Thread.Sleep(500);
+
+            //点击自动战斗
+            this.dm.ClickFoundPoint();
+            Thread.Sleep(5000);
+
+            DateTime startDatetime = DateTime.Now;
+            do
+            {
+                //等待恢复成 ‘自动战斗’
+                waiting.SetFinder(Finders.Zidonzhandou).WaitShow();
+                Thread.Sleep(500);
+
+                TimeSpan timeSpan = DateTime.Now - startDatetime;
+                if (timeSpan.Hours >= 1)
+                    return;
+                else
+                    Controller.ViewPrintLine("检测到当前计时未满足1小时。与起始时间的分钟差：{0}",timeSpan.Minutes);
+
+                //点击随机传送
+                waiting.BackClick(432, 611);
+                Thread.Sleep(500);
+
+                //点击自动战斗
+                this.dm.Click(458, 575);
+                Thread.Sleep(5000);
+            } while (true);
+        }
+        void jingji()
+        {
+            Waiting waiting = new Waiting();
+
+            //如果竞技有红点则点它
+            Finders.JingjiRedPoint();
+            if (this.dm.FindSuccess)
+                waiting.BackClick(227, 607);
+            else
+                return;
+            Thread.Sleep(500);
+
+            //等待野战PK页打开
+            waiting.SetFinder(Finders.YezhanPk).WaitShow();
+            Thread.Sleep(500);
+
+            //如果‘野战PK’有红点的话
+            if (Finders.YezhanPkRePoint())
+            {
+                //点击挑战
+                waiting.BackClick(422, 355);
+                Thread.Sleep(10000);
+
+                //等待到主界面
+                waiting.SetFinder(Finders.MainView).WaitShow(null, waiting.NewBackChecker(Finders.YezhanPkRePoint));
+            }
+            else if (Finders.WangzhezhenbaRedPoint())
+            {
+                //点击王者争霸红点
+                waiting.BackClickFoundPoint();
+                Thread.Sleep(500);
+
+                //等待匹配对手界面
+                waiting.SetFinder(Finders.Pipeiduishou).WaitShow(waiting.BackChecker);
+                Thread.Sleep(500);
+
+                //点击匹配对手
+                waiting.BackClickFoundPoint();
+                Thread.Sleep(500);
+
+                //等待到主界面
+                waiting.SetFinder(Finders.MainView).WaitShow(null, waiting.NewBackChecker(Finders.Pipeiduishou));
+            }
+        }
+        void quanmin()
+        {
+            Waiting waiting = new Waiting();
+
+            //点击BOOS
+            waiting.BackClick(454, 406);
+            Thread.Sleep(500);
+            
+            //
+            //根据红点检测是否有挑战次数
+            //
+            
+
+            //等待魔界界面（以前往挑战作为标识）
+            waiting.SetFinder(Finders.Qianwantiaozhan).WaitShow(null, waiting.NewBackChecker(Finders.MainView));
+            Thread.Sleep(500);
+
+            //点击全民选项
+            waiting.BackClick(192, 701);
+            Thread.Sleep(500);
+
+            //等待‘全民BOSS’界面
+            waiting.SetFinder(Finders.Tiaozhan).WaitShow(null, waiting.BackChecker);
+            Thread.Sleep(500);
+
+            //点击最后一项挑战
+            waiting.BackClick(425, 621);
+            Thread.Sleep(500);
+
+            //勾选自动复活
+            waiting.SetFinder(Finders.Zidonfuhuo).WaitShow(null, waiting.BackChecker);
+            waiting.BackClick(0,0);
+
+            //等待回到主界面..
+        }
+        void shenyu()
+        {
+            //点击BOOS挑战
+
+            //点击神域选项
+
+            //如果有挑战次数：
+
+            //点击最后一项挑战
+
+            //勾选自动复活
+
+            //等待回到主界面..
+        }
+    }
+}
+
