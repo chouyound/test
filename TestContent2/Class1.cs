68dc3f8c37c541724a87fa1d1665a8ad5b288d3f
chouyoun <chouyoun@foxmail.com>
2020-03-13 22:51:07 +0800

    fuck git add TestContent2

---
+++ TestContent2/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Baymax;
+
+namespace TestContent2
+{
+    [Description("测试脚本112")]
+    public class Class1 : IContent
+    {
+        public ISimulator Simulator
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public ThreadSignalHandler ThreadSignalHandler
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Hwnd
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string SourceFile
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Main(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ThreadExiting()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
