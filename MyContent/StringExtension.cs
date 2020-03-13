5121991e5633a6054b5fa407e913f256748e0342
chouyoun <chouyoun@foxmail.com>
2020-03-13 21:35:00 +0800

    aa

---
+++ MyContent/StringExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MyContent
+{
+    public static class StringExtension
+    {
+        public static bool RegexMatch(this string me,string pat)
+        {
+            return Regex.Match(me, pat).Success;
+        }
+    }
+}
